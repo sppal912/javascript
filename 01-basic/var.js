3a63bd6538175200daa25101e0fa580fee30ac1c
sppal912 <souvikpal9123370318@gmail.com>
2026-04-24 12:00:57 +0000

    new file var.js created

---
+++ 01-basic/var.js
@@ -0,0 +1,10 @@
+const name = 'souvik' //const is constant can not changeable
+let age = 29 // let is a variable it can changeable
+var city = 'kolkata' // var is very old not in use
+state = 'west bengal' // bad coder practices
+
+// name = 'rahul' (not posible it is a constant)
+age = 25
+city= 'mumbai'
+state= 'maharastra'
+console.table([name,age,city,state])
