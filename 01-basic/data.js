50dd8bd521a7a6781450cf1709947473a5e17c80
sppal912 <souvikpal9123370318@gmail.com>
2026-04-24 12:16:29 +0000

    data types of javascript

---
+++ 01-basic/data.js
@@ -0,0 +1,9 @@
+// number = 1 to 10   Used for integers and decimals
+// String = 'souvik'   Text data
+// Boolean = true/false   True or false
+// BigInt = 1n Very   large numbers
+// symbol = unique   Unique identifiers
+// undefined = undefined   Variable declared but no value
+// null = empty value    Intentional empty value
+
+console.log(typeof undefined)
