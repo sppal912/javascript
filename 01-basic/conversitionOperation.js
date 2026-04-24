a9c7f4c3b8834d58abc492e9e17bc3d4273e21c7
sppal912 <souvikpal9123370318@gmail.com>
2026-04-24 13:31:13 +0000

    conversion and Oporation

---
+++ 01-basic/conversitionOperation.js
@@ -0,0 +1,36 @@
+let score = '33'
+// console.log(typeof score) // we will know what type of data type is this
+
+let valueInNumber = Number(score) // it will convert string to number
+// console.log(valueInNumber)
+
+// console.log(typeof score)
+/* '33' => 33
+'33abc'=> NaN
+true => 1; false => 0 */
+
+let loggedIn = 'souvik'
+let booleanIsLoggedIn = Boolean(loggedIn) //it will convart to boolean
+// console.log(booleanIsLoggedIn) //it return true if it have some value 
+
+let myNumber = 58
+let numberToString = String(myNumber)
+// console.log(typeof numberToString)
+
+// *********************************************** Operations ****************************************************
+
+let value= 3
+let negvalue = -value // it will give negetive value
+// console.log(negvalue)
+
+// console.log(2+2) // addition
+// console.log(2-2) // subtraction
+// console.log(2*2) // multiplication
+// console.log(2**3) // 2 power 3
+// console.log(2/3) // devide
+// console.log(2%3) // remander
+
+
+ let point = 100
+ point++
+ console.log(point)
