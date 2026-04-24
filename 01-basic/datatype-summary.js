7ee43a31fa175b1ee1559ab26bce5261a648c5e4
sppal912 <souvikpal9123370318@gmail.com>
2026-04-24 14:51:39 +0000

    hear id data type summary

---
+++ 01-basic/datatype-summary.js
@@ -0,0 +1,22 @@
+// Primitive
+// 7 types : string, number, boolean, null, undefined, symbol, bigint
+
+// non primitive
+
+// array, objects, functions
+const id = Symbol('123')
+const aid = Symbol('123')
+// console.log(id == aid)
+
+const bignumber = 64565169651n
+
+const heros = ['ram','sam','jadu'] // this is array
+let myobj ={
+    name: 'souvik',
+    age: 29
+}// this is object 
+
+const myFunc = function(){
+    console.log('helo world')
+} // this is function 
+// console.log(typeof bignumber)
