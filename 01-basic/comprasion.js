1db7cf4435f996c5135d6e1674c26fe3273e8597
sppal912 <souvikpal9123370318@gmail.com>
2026-04-24 13:57:27 +0000

    comprasion

---
+++ 01-basic/comprasion.js
@@ -0,0 +1,15 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+/* //   console.log('1'>1)
+//   console.log('02'>1)
+//   console.log(null > 0)
+//   console.log(null == 0)
+//   console.log(null >= 0)
+//   console.log(null = 0) */ // avoid this type of convertion
+
+// let number = null
+// console.log(typeof number)
