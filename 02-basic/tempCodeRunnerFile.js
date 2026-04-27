6f3d92d780c6f4ca6bf07b71f3b3734009c2b180
sppal912 <souvikpal9123370318@gmail.com>
2026-04-27 09:05:11 +0000

    temp

---
+++ 02-basic/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+ // in this methourd we can change name in side the obj
+// //  Object.freeze(jsUser) // we freeze this object jsUser becuse we don't want to change object key's
+//  jsUser.name= 'sayam' 
