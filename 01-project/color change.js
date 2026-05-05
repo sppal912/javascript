f693c439247806933ce5168db7431c97028b7d28
sppal912 <souvikpal9123370318@gmail.com>
2026-05-05 14:16:27 +0000

    project no 1

---
+++ 01-project/color change.js
@@ -0,0 +1,19 @@
+const body = document.querySelector('body')
+const btn = document.querySelectorAll('.button')
+
+btn.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
