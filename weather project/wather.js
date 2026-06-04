9eb929c51910e418c9ae805bbc562ed434a6c2b4
sppal912 <souvikpal9123370318@gmail.com>
2026-06-04 16:19:38 +0000

    weather app update

--- weather project/wather.js
+++ weather project/wather.js
@@ -1,15 +1,15 @@
-
-
 const searchBtn = document.querySelector("#search-btn");
 
 const apiKey = "a976ab6cf442cc145e867636f49c60c4";
+const cityInput = document.querySelector("#city-input");
 
+cityInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    getWeather();
+  }
+});
 searchBtn.addEventListener("click", getWeather);
-
 async function getWeather() {
-  const cityInput = document.querySelector("#city-input");
-  const stateInput = document.querySelector("#state-input");
-  const countryInput = document.querySelector("#country-input");
 
   const cityName = document.querySelector("#city-name");
   const temp = document.querySelector("#temperature");
@@ -17,6 +17,8 @@ async function getWeather() {
   const humidity = document.querySelector("#humidity");
   const windSpeed = document.querySelector("#wind-speed");
   const weatherIcon = document.querySelector("#weather-icon");
+  const sunrise = document.querySelector("#sunrise");
+  const sunset = document.querySelector("#sunset");
 
   if (!cityInput.value.trim()) {
     cityInput.value = "";
@@ -25,37 +27,24 @@ async function getWeather() {
     return;
   }
 
-  if (!stateInput.value.trim()) {
-    stateInput.value = "";
-    stateInput.placeholder = "Enter state code";
-    stateInput.focus();
-    return;
-  }
-
-  if (!countryInput.value.trim()) {
-    countryInput.value = "";
-    countryInput.placeholder = "Enter country code";
-    countryInput.focus();
-    return;
-  }
-
   const city = cityInput.value.trim();
-  const stateCode = stateInput.value.trim().toUpperCase();
-  const countryCode = countryInput.value.trim().toUpperCase();
-
+  cityName.textContent = "Loading...";
   try {
     // Step 1: Get Latitude & Longitude
-    const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city},${stateCode},${countryCode}&limit=1&appid=${apiKey}`;
+    const getCityDetailsUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`;
 
-    const geoResponse = await fetch(geoUrl);
+    const geoResponse = await fetch(getCityDetailsUrl);
     const geoData = await geoResponse.json();
 
     if (!geoData.length) {
       cityName.textContent = "City not found";
+      setTimeout(() => {
+        cityName.textContent = "- -";
+      }, 2000);
       return;
     }
 
-    const { lat, lon } = geoData[0];
+    const { lat, lon, country } = geoData[0];
 
     // Step 2: Get Weather Data
     const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
@@ -64,7 +53,7 @@ async function getWeather() {
     const weatherData = await weatherResponse.json();
 
     // Step 3: Update UI
-    cityName.textContent = weatherData.name;
+    cityName.textContent = `${weatherData.name}, ${country}`;
 
     temp.textContent = `${Math.round(weatherData.main.temp)}°C`;
 
@@ -72,16 +61,63 @@ async function getWeather() {
 
     humidity.textContent = `${weatherData.main.humidity}%`;
 
-    windSpeed.textContent = `${weatherData.wind.speed} m/s`;
+    windSpeed.textContent = `${Math.round(weatherData.wind.speed * 3.6)} km/h`;
+
+    const sunriseTime = new Date(weatherData.sys.sunrise * 1000);
+    const sunsetTime = new Date(weatherData.sys.sunset * 1000);
+
+    sunrise.textContent = sunriseTime.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+
+    sunset.textContent = sunsetTime.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
 
     const iconCode = weatherData.weather[0].icon;
 
     weatherIcon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
 
     weatherIcon.alt = weatherData.weather[0].description;
+    // clear input field after search
+    cityInput.value = "";
   } catch (error) {
-    console.error(error);
+    console.log(error);
     cityName.textContent = "Failed to fetch weather data";
   }
 }
 
+// api result
+// {
+//   coord: { lon: 88.36, lat: 22.57 },
+//   weather: [
+//     {
+//       id: 802,
+//       main: 'Clouds',
+//       description: 'scattered clouds',
+//       icon: '03n'
+//     }
+//   ],
+//   base: 'stations',
+//   main: {
+//     temp: 27.74,
+//     feels_like: 31.32,
+//     temp_min: 27.74,
+//     temp_max: 27.74,
+//     pressure: 1002,
+//     humidity: 79,
+//     sea_level: 1002,
+//     grnd_level: 1001
+//   },
+//   visibility: 10000,
+//   wind: { speed: 11.91, deg: 245, gust: 17.98 },
+//   clouds: { all: 26 },
+//   dt: 1780579841,
+//   sys: { country: 'IN', sunrise: 1780528894, sunset: 1780577303 },
+//   timezone: 19800,
+//   id: 1275004,
+//   name: 'Kolkata',
+//   cod: 200
+// }
